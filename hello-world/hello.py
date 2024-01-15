a7d688d36958b6ffe269c32d09c374f831409cee
Juhana <61124548+WelehoBRUDER@users.noreply.github.com>
2024-01-15 14:56:26 +0200

    init

---
+++ hello-world/hello.py
@@ -0,0 +1,11 @@
+print("Hello world!")
+
+
+def greet_someone(name):
+    print("Have a great day {}!".format(name))
+
+
+input_name = input("What is your name?")
+greet_someone(input_name)
+
+# I am clearly an incredible python programmer
