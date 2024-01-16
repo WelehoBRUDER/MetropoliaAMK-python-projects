7e35e059a8de5be4d4ed9be27da2ec11d90b4c28
Juhana <61124548+WelehoBRUDER@users.noreply.github.com>
2024-01-16 09:49:19 +0200

    very important commit

--- hello-world/hello.py
+++ hello-world/hello.py
@@ -1,11 +1,26 @@
 print("Hello world!")
 
 
-def greet_someone(name):
-    print("Have a great day {}!".format(name))
+def greet_someone():
+    input_name = input("What is your name?\t")
+    # Define minimum length of name
+    name_min_length = 2
+    # Define maximum length of name
+    name_max_length = 30
+    # Prevent empty prints when length of name is wrong
+    dont_print = False
+    if len(input_name) < name_min_length or len(input_name) > name_max_length:
+        if len(input_name) < name_min_length:
+            print("Too short!")
+        if len(input_name) > name_max_length:
+            print("Too long!")
+        greet_someone()
+        dont_print = True
+
+    if not dont_print:
+        print("Have a great day {}!".format(input_name))
 
 
-input_name = input("What is your name?\t")
-greet_someone(input_name)
+greet_someone()
 
 # I am clearly an incredible python programmer
