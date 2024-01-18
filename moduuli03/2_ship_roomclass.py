486a7179acc10a7fa3c0dda60c683962082d8f45
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-01-18 15:28:30 +0200

    ship roomclasses

---
+++ moduuli03/2_ship_roomclass.py
@@ -0,0 +1,14 @@
+roomclasses = {
+    "LUX": "LUX on parvekkeellinen hytti yläkannella.",
+    "A": "A on ikkunallinen hytti autokannen yläpuolella.",
+    "B": "B on ikkunaton hytti autokannen yläpuolella.",
+    "C": "C on ikkunaton hytti autokannen alapuolella."
+}
+
+selected_room = input("Valitse hyttiluokka (LUX, A, B, C): ")
+# Make sure user input is in caps
+selected_room = selected_room.upper()
+if selected_room in roomclasses:
+    print(roomclasses[selected_room])
+else:
+    print("Virheellinen hyttiluokka")
