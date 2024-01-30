69e8ac1d00cb0e106e7b7c18c379c5930697b023
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-01-30 11:03:54 +0200

    idk

--- moduuli04/6_pi_approximation.py
+++ moduuli04/6_pi_approximation.py
@@ -6,7 +6,7 @@ points_in_circle = 0
 
 for i in range(1, number_of_points):
     cords = {"x": random.uniform(-radius, radius), "y": random.uniform(-radius, radius)}
-    if cords["x"] ** 2 + cords["y"] ** 2 < radius:
+    if cords["x"] ** 2 + cords["y"] ** 2 <= radius:
         points_in_circle += 1
 
 pi_approximate = 4 * points_in_circle / number_of_points
--- moduuli06/2_dice_total.py
+++ moduuli06/2_dice_total.py
@@ -8,10 +8,13 @@ def roll_dice(sides):
 def main():
     dice_sides = int(input("Montako tahkoa nopassa on?: "))
 
+    times_rolled = 0
     roll = 0
     while roll != dice_sides:
+        times_rolled += 1
         roll = roll_dice(dice_sides)
         print(roll)
+    print(f"The dice was rolled {times_rolled} times before hitting {dice_sides}!")
 
 
 main()
