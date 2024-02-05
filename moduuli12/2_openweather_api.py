8ddaf0a4296638164cf3937f852e25d2c41c0c47
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-02-05 13:23:38 +0200

    openweather api key just dropped!!!!!!

--- moduuli12/2_openweather_api.py
+++ moduuli12/2_openweather_api.py
@@ -1,18 +1,35 @@
 import requests
+
 API_KEY = "7cc813f152116cb974a24d671691cf51"
 
+
 def geocode(city, state, country):
-  API_CALL = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=5&appid={API_KEY}"
-  return requests.get(API_CALL)
-  
+    api_call = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=5&appid={API_KEY}"
+    res = requests.get(api_call)
+    return {"lon": res.json()[0]["lon"], "lat": res.json()[0]["lat"]}
+
 
 def call_weather(lat, lon):
-  API_CALL = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
+    api_call = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
+    res = requests.get(api_call)
+    return res.json()
+
 
 def main():
-  DEFAULT_COUNTRY = "FI"
-  DEFAULT_STATE = "Finland"
-  test_loc = geocode("Helsinki",DEFAULT_STATE,DEFAULT_COUNTRY)
-  print(test_loc.json())
-  
+    default_country = "FI"
+    default_state = "Finland"
+    city = input("Minkä paikkakunnan sään haluat tietää?: ")
+
+    loc = geocode(city, default_state, default_country)
+    weather_call = call_weather(loc["lat"], loc["lon"])
+    print(weather_call)
+    weather = weather_call["weather"][0]
+    # As Kelvin starts at absolute zero (-273.15C), we must offset the value.
+    temp = weather_call["main"]["temp"] - 273.15
+    print(weather)
+
+    print(f"Paikkakunnan {city} säätila on:")
+    print(f"{weather['main']}: {weather['description']}\nLämpötila: {'{:.2f}'.format(temp)} C")
+
+
 main()
